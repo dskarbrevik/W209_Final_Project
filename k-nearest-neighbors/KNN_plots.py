ccd4f1674417d5760a54641a434b31d17cf5e3e0
David Skarbrevik <skarbrevik@ischool.berkeley.edu>
2017-03-28 14:21:08 -0700

    updated KNN bokeh plot
    Decision Boundary w/ Brad's colors added. Will be adding interaction to
    change "k" value next.

--- k-nearest-neighbors/KNN_plots.py
+++ k-nearest-neighbors/KNN_plots.py
@@ -1,4 +1,4 @@
-from bokeh.plotting import figure, output_file, show
+from bokeh.plotting import figure, output_file, show, reset_output
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report
 from sklearn import datasets
@@ -8,6 +8,10 @@ from matplotlib.colors import ListedColormap
 from bokeh.charts import Scatter, output_file, show
 from bokeh.models import ColumnDataSource
 from bokeh.sampledata.autompg import autompg as df
+from bokeh.io import output_file, show, curdoc
+from bokeh.layouts import widgetbox, layout, gridplot, row
+from bokeh.models.widgets import Slider
+from bokeh.models.mappers import LinearColorMapper
 
 #np.set_printoptions(threshold=np.inf)
 
@@ -31,7 +35,7 @@ test_labels = iris.target[120:]
 #    else:	            
 #        test_data = np.append(test_data, iris.data[x])
 #        test_labels = np.append(test_labels, iris.target[x])
-#
+
 #type(train_data)
 #type(train_labels)
 #type(test_data)
@@ -61,81 +65,13 @@ test_labels = iris.target[120:]
 #################################################
 
 
-kneighbor = KNeighborsClassifier() # create the classifier
+kneighbor = KNeighborsClassifier(n_neighbors=100) # create the classifier
 kneighbor.fit(train_data[:,:2], train_labels) # train the classifier
 #preds = kneighbor.predict(test_data) # test the classifier
 
-#################################################
-# MATPLOTLIB IMPLEMENTATION
-#################################################
-#cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
-#cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 
-#h = 0.01 #step size in mesh
-#
-## Plot the decision boundary. For that, we will assign a color to each
-## point in the mesh [x_min, x_max]x[y_min, y_max].
-#x_min, x_max = test_data[:, 0].min() - 1, test_data[:, 0].max() + 1
-#y_min, y_max = test_data[:, 1].min() - 1, test_data[:, 1].max() + 1
-#xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-#                     np.arange(y_min, y_max, h))
-#preds = kneighbor.predict(np.c_[xx.ravel(), yy.ravel()])
-#
-## Put the result into a color plot
-#preds = preds.reshape(xx.shape)
-#
-#plt.figure()
-#plt.pcolormesh(xx, yy, preds, cmap=cmap_light)
-#
-## Plot also the training points
-#plt.scatter(test_data[:, 0], test_data[:, 1], c=test_labels, cmap=cmap_bold)
-#plt.xlim(xx.min(), xx.max())
-#plt.ylim(yy.min(), yy.max())
-##plt.title("3-Class classification (k = %i, weights = '%s')"
-##          % (n_neighbors, weights))
-#
-#
-#plt.show()
 
-###########################################
 
-### Stole this def that extracts matplotlib data
-
-def get_contour_data(X, Y, Z):
-    cs = plt.contour(X, Y, Z)
-    xs = []
-    ys = []
-    xt = []
-    yt = []
-    col = []
-    text = []
-    isolevelid = 0
-    for isolevel in cs.collections:
-        isocol = isolevel.get_color()[0]
-        thecol = 3 * [None]
-        theiso = str(cs.get_array()[isolevelid])
-        isolevelid += 1
-        for i in range(3):
-            thecol[i] = int(255 * isocol[i])
-        thecol = '#%02x%02x%02x' % (thecol[0], thecol[1], thecol[2])
-
-        for path in isolevel.get_paths():
-            v = path.vertices
-            x = v[:, 0]
-            y = v[:, 1]
-            xs.append(x.tolist())
-            ys.append(y.tolist())
-            xt.append(x[len(x) / 2])
-            yt.append(y[len(y) / 2])
-            text.append(theiso)
-            col.append(thecol)
-
-    source = ColumnDataSource(data={'xs': xs, 'ys': ys, 'line_color': col,'xt':xt,'yt':yt,'text':text})
-    return source
-
-##############################################
-
-#from bokeh.sampledata.iris import flowers as df
 
 
 # prepare some data
@@ -145,8 +81,8 @@ y = train_data[:, 1]
 
 x_min, x_max = train_data[:, 0].min() - 1, train_data[:, 0].max() + 1
 y_min, y_max = train_data[:, 1].min() - 1, train_data[:, 1].max() + 1
-xx, yy = np.meshgrid(np.arange(x_min, x_max, .02),
-                     np.arange(y_min, y_max, .02))
+xx, yy = np.meshgrid(np.arange(x_min, x_max, .001),
+                     np.arange(y_min, y_max, .001))
 
 
 Z = kneighbor.predict(np.c_[xx.ravel(), yy.ravel()])
@@ -154,16 +90,32 @@ Z = kneighbor.predict(np.c_[xx.ravel(), yy.ravel()])
 # Put the result into a color plot
 Z = Z.reshape(xx.shape)
 
-source = get_contour_data(xx,yy,Z)
+cmap_light = ['#FFAAAA', '#AAFFAA', '#AAAAFF']
+cmap_bold = ['#FF0000', '#00FF00', '#0000FF']
 
+from bokeh.palettes import Category20
+light_palette = [Category20[6][2*i + 1] for i in range(3)]
+dark_palette = [Category20[6][2*i] for i in range(3)]
 
 p = figure()
-p.multi_line(xs='xs', ys='ys', line_color='line_color', source=source)
-colormap = {0: 'red', 1: 'green', 2: 'blue'}
-colors = [colormap[x] for x in train_labels]
-p.circle(x,y, color=colors, radius=.02)
-show(p)
+p.image(image=[Z],x=x_min, y=y_min, dw=(x_max-x_min), dh=(y_max-y_min), palette=light_palette)
 
+
+#source = get_contour_data(xx,yy,Z)
+#
+#
+#p = figure()
+#p.multi_line(xs='xs', ys='ys', line_color='line_color', source=source)
+colormap = {0: 'red', 1: 'green', 2: 'blue'}
+colors = [dark_palette[x] for x in train_labels]
+p.circle(x,y, color=colors, radius=.02)
+##show(p)
+#
+slider = widgetbox(Slider(start=0, end=10, value=1, step=1, title="K = ?"))
+##reset_output
+l = row(slider,p,)
+#
+show(l)
 # output to static HTML file
 #output_file("lines.html")
 
